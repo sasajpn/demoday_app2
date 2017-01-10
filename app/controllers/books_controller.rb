7f4b33ed4b92a76d295fcf2bbe800c167d2f23da
Kazuki Sasahara <kazuki@KazukinoMacBook-Pro.local>
2017-01-11 06:55:35 +0900

    book CRUD作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,52 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_book, only: [:destroy]
+
+  def new
+    @books = []
+    if params[:keyword].present?
+      Amazon::Ecs.debug = true
+      books = Amazon::Ecs.item_search(
+        params[:keyword],
+        search_index: 'Books',
+        dataType: 'script',
+        response_group: 'ItemAttributes, Images',
+        country: 'jp',
+        power: "Not kindle"
+      )
+      books.items.each do |item|
+        book = current_user.books.build(
+          title: item.get('ItemAttributes/Title'),
+          author: item.get('ItemAttributes/Author'),
+          image: item.get('LargeImage/URL')
+        )
+        @books << book
+      end
+    end
+  end
+
+  def create
+    @book = current_user.books.build(book_params)
+    if @book.save
+      redirect_to user_url(current_user)
+    else
+      @books = []
+      render :new
+    end
+  end
+
+  def destroy
+    @book.destroy
+    redirect_to user_url(current_user)
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :author, :image, :status, :user_id)
+  end
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
 end
