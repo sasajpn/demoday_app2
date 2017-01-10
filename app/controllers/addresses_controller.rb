bc9e55954f0536a41e267a2a397c1500dbb4a84b
Kazuki Sasahara <kazuki@KazukinoMacBook-Pro.local>
2017-01-11 06:18:51 +0900

    address CRUD作成

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,2 +1,43 @@
 class AddressesController < ApplicationController
+  before_action :set_address, except: [:new, :create]
+
+  def new
+    @address = current_user.addresses.build
+  end
+
+  def create
+    @address = current_user.addresses.build(address_params)
+    if @address.save
+      redirect_to user_url(current_user)
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @address.update(address_params)
+      redirect_to user_url(current_user)
+    end
+  end
+
+  def destroy
+    @address.destroy
+    redirect_to user_url(current_user)
+  end
+
+  private
+
+  def address_params
+    params.require(:address).permit(
+      :user_id,
+      :given_name, :family_name,
+      :postal_code, :prefecture, :municipality, :street, :building)
+  end
+
+  def set_address
+    @address = Address.find(params[:id])
+  end
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def user_check
+    @user = User.find(params[:id]) || User.find(params[:user_id])
+    redirect_to user_url(current_user), notice: "そのページはご利用いだだけません" unless current_user == @user
+
+  private
+
+  def configure_permitted_parameters
+    added_attrs = [:username, :email, :birthday, :password, :password_confirmation]
+    devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+  end
 end
