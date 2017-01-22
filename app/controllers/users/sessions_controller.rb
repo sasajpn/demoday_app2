6091ad277ea03e613d4f224ac6be561e2bb8ffbf
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2017-01-22 15:09:43 +0900

    ログイン・ログアウト後のurl変更

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,3 +1,11 @@
 class Users::SessionsController < Devise::SessionsController
   layout 'mypage/devise'
+
+  def after_sign_in_path_for(resource)
+    user_url(current_user)
+  end
+
+  def after_sign_out_path_for(resource)
+    root_url
+  end
 end
