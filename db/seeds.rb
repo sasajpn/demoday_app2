51cfa7c16688edfdc089af2231b6453b17d7bc61
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2017-01-22 15:01:56 +0900

    seedファイル編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,14 +12,14 @@ CSV.foreach('db/prefecturals_name_seed.csv') do |row|
   namelist[name.id] = name
 end
 
-Area.delete_all
-CSV.foreach('db/postal_code_seed.csv') do |row|
-  record = {
-    postal_code:  row[0],
-    prefecture:   namelist[row[1].to_i],
-    municipality: row[3],
-    street:       row[4]
-  }
-  p record
-  Area.create!(record)
-end
+# Area.delete_all
+# CSV.foreach('db/postal_code_seed.csv') do |row|
+#   record = {
+#     postal_code:  row[0],
+#     prefecture:   namelist[row[1].to_i],
+#     municipality: row[3],
+#     street:       row[4]
+#   }
+#   p record
+#   Area.create!(record)
+# end
