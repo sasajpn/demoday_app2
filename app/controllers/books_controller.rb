b3aadaff8a7b0950664999eff6756062ebb8a42c
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2017-01-25 00:12:23 +0900

    amazon検索エラーの時の処理

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,23 +10,34 @@ class BooksController < ApplicationController
 
   def new
     @books = []
-    if params[:keyword].present?
-      Amazon::Ecs.debug = true
-      books = Amazon::Ecs.item_search(
-        params[:keyword],
-        search_index: 'Books',
-        dataType: 'script',
-        response_group: 'ItemAttributes, Images',
-        country: 'jp',
-        power: 'Not kindle'
-      )
-      books.items.each do |item|
-        book = current_user.books.build(
-          title: item.get('ItemAttributes/Title'),
-          author: item.get('ItemAttributes/Author'),
-          image: item.get('LargeImage/URL')
+    retry_count = 0
+    begin
+      if params[:keyword].present?
+        Amazon::Ecs.debug = true
+        books = Amazon::Ecs.item_search(
+          params[:keyword],
+          search_index: 'Books',
+          dataType: 'script',
+          response_group: 'ItemAttributes, Images',
+          country: 'jp',
+          power: 'Not kindle'
         )
-        @books << book
+        books.items.each do |item|
+          book = current_user.books.build(
+            title: item.get('ItemAttributes/Title'),
+            author: item.get('ItemAttributes/Author'),
+            image: item.get('LargeImage/URL')
+          )
+          @books << book
+        end
+      end
+    rescue
+      retry_count += 1
+      if retry_count < 5
+        sleep(5)
+      else
+        flash[:alert] = 'しばらく経ってからもう一度検索してください'
+        render :new
       end
     end
   end
