36ff60dad4efade392ef9c75f13972174909a319
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2017-02-18 10:16:10 +0900

    アイテム一覧を作成

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,9 @@
+class ItemsController < ApplicationController
+  skip_before_action :user_check, :user_book_check
+
+  def index
+    @items = Book.where(status: 0).where.not(user_id: current_user)
+    @search = @items.search(params[:q])
+    @search_items = @search.result(distinct: true)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -23,4 +23,5 @@ Rails.application.routes.draw do
     end
   end
   resources :deals, only: [:index]
+  resources :items, only: [:index]
 end
