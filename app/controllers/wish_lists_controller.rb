19d3010e7db4a27501aff1abbd119cfbe7a207f5
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2017-02-25 12:14:49 +0900

    ほしいものリスト一覧を作成

--- app/controllers/wish_lists_controller.rb
+++ app/controllers/wish_lists_controller.rb
@@ -1,6 +1,6 @@
 class WishListsController < ApplicationController
 
   def index
-    @wish_list = current_user.wish_lists.order(created_at: :desc)
+    @wish_lists = current_user.wish_lists.order(created_at: :desc)
   end
 end
