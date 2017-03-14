47f522e11d057f8c68edbe8ffd0855e9676720fb
Kazuki Sasahara <s.kazuki.japan@gmail.com>
2017-03-14 22:20:56 +0900

    交換決定後、trade作成

--- app/models/reply.rb
+++ app/models/reply.rb
@@ -4,12 +4,20 @@ class Reply < ActiveRecord::Base
 
   validates :wish_id, uniqueness: { scope: :book_id }
 
-  after_update :decide_change
+  after_update :decide_change, :create_trade
 
   private
 
   def decide_change
-    book.update(change: true)
-    wish.book.update(change: true)
+    if status == 2
+      book.update(change: true)
+      wish.book.update(change: true)
+    end
+  end
+
+  def create_trade
+    if status == 2
+      Trade.create(wish_book_id: wish.book, reply_book_id: book)
+    end
   end
 end
